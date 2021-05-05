726fefb1b9e45a4fa7258266b879a84f552fbe33
Steven Boulom <sboulom@gmail.com>
2021-05-05 07:54:20 -0700

    assignment 1

---
+++ reactWeek1Ass.js
@@ -0,0 +1,26 @@
+'use strict';
+
+class Student {
+    constructor(name, email, community){
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+
+class Bootcamp {
+    constructor(name, level, students = []){
+        this.name = name;
+        this.level = level;
+        this.students = students;
+
+        if(students == 'undefined'){
+            students == 'none'
+        }
+    }
+
+    registerStudent(student){
+        
+    }
+}
+
