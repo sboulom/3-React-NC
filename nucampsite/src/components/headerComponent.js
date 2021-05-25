4f544fa0524a0f613478b215a9125a4e28881003
Steven Boulom <sboulom@gmail.com>
2021-05-24 20:42:06 -0700

    fixed header and main component

--- nucampsite/src/components/headerComponent.js
+++ nucampsite/src/components/headerComponent.js
@@ -9,7 +9,7 @@ class Header extends Component {
         this.state = {
             isNavOpen: false
         }
-    }
+    } 
 
     toggleNav(){
         this.setState({
@@ -49,7 +49,7 @@ class Header extends Component {
                                 </NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink className="nav-link" to="/aboutus">
+                            <NavLink className="nav-link" to="/about">
                                 <i className="fa fa-info fa-lg" /> About
                                 </NavLink>
                         </NavItem>
