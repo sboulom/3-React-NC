3430a7c99008aa50dd70d170c0e3b674887832be
Steven Boulom <sboulom@gmail.com>
2021-05-31 23:56:59 -0700

    render comments

--- nucampsite/src/components/campsiteInfoComponent.js
+++ nucampsite/src/components/campsiteInfoComponent.js
@@ -22,8 +22,8 @@ class CommentForm extends Component{
         });
     }
     handleSubmit(values){
-        console.log("Current state is: " + JSON.stringify(values));
-        alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
     }
     
     render(){
@@ -112,7 +112,7 @@ class CommentForm extends Component{
             </div>
         )
     }
-    function RenderComments({comments}){
+    function RenderComments({comments, addComment, campsiteId}){
         if(comments){
             return (
             <div className='col-md-5 m-1'>
@@ -127,7 +127,7 @@ class CommentForm extends Component{
                         </div>
                     )
                 })}
-                <CommentForm />
+                <CommentForm campsiteId={campsiteId} addComment={addComment}/>
             </div>)
         }
     }
@@ -147,7 +147,11 @@ class CommentForm extends Component{
                     </div>
                     <div className="row">
                         <RenderCampsite campsite={props.campsite} />
-                        <RenderComments comments={props.comments} />
+                        <RenderComments 
+                            comments={props.comments}
+                            addComment={props.addComment}
+                            campsiteId={props.campsite.id}
+                        />
                     </div>
                 </div>   
            )
--- nucampsite/src/components/mainComponent.js
+++ nucampsite/src/components/mainComponent.js
@@ -8,6 +8,7 @@ import Contact from './contactComponent';
 import About from './aboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment} from '../redux/ActionCreators'
 // import { CAMPSITES } from '../shared/campsites';
 // import { COMMENTS } from '../shared/comments';
 // import { PARTNERS } from '../shared/partners';
@@ -21,6 +22,10 @@ const mapStateToProps = state => {
         partners: state.partners, 
         promotions: state.promotions
     }
+}
+
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
 }
 
 class Main extends Component {
@@ -48,6 +53,7 @@ class Main extends Component {
                 <CampsiteInfo 
                     campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                     comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+                    addComment={this.props.addComment}
                 />
             );
         } 
@@ -69,4 +75,4 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
