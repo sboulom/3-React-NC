8cb0ee90c97c311dccf806f2e0c130b889642cb0
Steven Boulom <sboulom@gmail.com>
2021-06-01 00:54:06 -0700

    added data to persist once changing page

--- nucampsite/src/components/mainComponent.js
+++ nucampsite/src/components/mainComponent.js
@@ -8,6 +8,7 @@ import Contact from './contactComponent';
 import About from './aboutComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import{ actions } from 'react-redux-form';
 import { addComment, fetchCampsites} from '../redux/ActionCreators'
 // import { CAMPSITES } from '../shared/campsites';
 // import { COMMENTS } from '../shared/comments';
@@ -26,7 +27,8 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = {
     addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
-    fetchCampsites: () => (fetchCampsites())
+    fetchCampsites: () => (fetchCampsites()),
+    resetFeedbackForm: () => (actions.reset('feedbackForm'))
 }
 
 class Main extends Component {
@@ -75,7 +77,7 @@ class Main extends Component {
                     } />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route path='/about' render={()=><About partners={this.props.partners}/>} />
-                    <Route exact path = '/contactus' component={Contact} />
+                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
